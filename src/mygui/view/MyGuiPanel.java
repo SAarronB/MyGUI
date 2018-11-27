e558e96d1112f53a86d0b44422868ab90e1ec017
SAaronB <programmingsean@gmail.com>
2018-11-27 08:54:26 -0700

    Added All Kinds of Listeners, button and label

--- src/mygui/view/MyGuiPanel.java
+++ src/mygui/view/MyGuiPanel.java
@@ -1,35 +1,46 @@
 package mygui.view;
 
 import mygui.controller.GuiController;
+import java.awt.Color;
 import javax.swing.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 //+--------------Needs to Extend JPanel-----------+
 public class MyGuiPanel extends JPanel{
 	private GuiController appController;
 	
+	private JButton colorButton;
+	private JLabel textLabel;
+	
 	public MyGuiPanel(GuiController appController) {
 		super();
 		this.appController = appController;
+		colorButton = new JButton("!!P0WER BUTT0N!!");
+		textLabel = new JLabel("PR3SS DA BUTT0N");
 		setupPanel();
 		setupLayout();
 		setupListener();
 	}
 
-	private void setupListener()
-	{
+	private void setupPanel(){
 		// TODO Auto-generated method stub
-		
+		this.setBackground(Color.DARK_GRAY);
+		this.add(textLabel);
+		this.add(colorButton);
 	}
-
+	
 	private void setupLayout()
 	{
 		// TODO Auto-generated method stub
 		
 	}
-
-	private void setupPanel()
+	
+	private void setupListener()
 	{
 		// TODO Auto-generated method stub
-		
+		colorButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent mouseClick) {}
+		});
 	}
 }
